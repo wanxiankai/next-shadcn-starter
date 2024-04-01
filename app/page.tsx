b261da19c48a4fd625b3915668c4e555456909a9
wanxiankai <wanxiankai@126.com>
2024-04-01 20:02:45 +0800

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="text-3xl font-bold">
+          Welcome to the Next.js + TS With Tailwind CSS Starter
+        </h1>
+      </div>
+    </section>
+  )
+}
